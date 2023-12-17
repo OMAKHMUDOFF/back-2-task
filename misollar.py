f3056f5d9406544b371dfd1a2e2b8aa4f51ef0a7
OMAKHMUDOFF <omaxmudovv@gmail.com>
2023-12-17 16:39:06 +0500

    backend 2-task

---
+++ misollar.py
@@ -0,0 +1,21 @@
+"""1 misol"""
+print(len(input("ism kiriting : ")))
+"""2 misol"""
+print(input("Tug'ilgan joyingizni kiriting : ") == "Toshkent")
+"""3 misol"""
+print(2023 - int(input("Tug'ilgan yilingizni kiriting : ")), "Yosh ekansiz")
+"""4 misol"""
+print("Siz", 2023 - int(input("Yoshingizni kiriting : ")), "yilda tug'ilgansiz")
+"""5 misol"""
+print("Tovuqlarning oyoqlari soni", int(input("Tovuqlar sonini kiriting : ")) * 2)
+"""6 misol"""
+print(f"Bitta faslda {12 // 4} oy bor")
+"""8 misol"""
+print(f"Mars sayyorasida {687 // 30}ta oy bor")
+"""9 misol"""
+print("Kiritilgan matn", len(input("Matn kiriting : ")) / 8, "bayt ga teng")
+"""10 misol"""
+xonadon1 = int(input("Birinchi xonadon raqamini kiritng : "))
+xonadon2 = int(input("Ikkinchi xonadon raqamini kiritng : "))
+masofa = (xonadon1 - xonadon2) * 40 / 1000
+print(f"Birinchi xonadondan ikkinchi xonadon orasidagi masofa {masofa}km")
